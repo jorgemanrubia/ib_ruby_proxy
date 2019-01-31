c2d968563246a6617c5bcce63292dc8e7bc56dc3
Jorge Manrubia <jorge.manrubia@gmail.com>
2019-01-31 09:44:06 +0100

    Remove minitest references. Add lib paths to rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,6 +13,10 @@
 # it.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+
+$LOAD_PATH << '../lib'
+require 'ib_ruby_proxy'
+
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
