f3ce0b7e7bc9b7a77dad73deb3fa724c9f179802
Jorge Manrubia <jorge.manrubia@gmail.com>
2019-02-02 18:24:00 +0100

    Copying to ib working

--- lib/ib_ruby_proxy/server/ib_ruby_class_generator.rb
+++ lib/ib_ruby_proxy/server/ib_ruby_class_generator.rb
@@ -14,6 +14,9 @@ module IbRubyProxy
         <<-RUBY
         #{generate_namespace_open}
         #{generate_class_declaration}
+          #{generate_constructor}
+          #{generate_to_ib_method}
+        end
         #{generate_namespace_close}
         RUBY
       end
@@ -23,8 +26,7 @@ module IbRubyProxy
       IB_FIELD_PREFIX = /^m_/
 
       def java_property_fields
-        ib_class.java_class.declared_fields.find_all {|field| field.name =~ IB_FIELD_PREFIX
-        }
+        @java_property_fields ||= ib_class.java_class.declared_fields.find_all {|field| field.name =~ IB_FIELD_PREFIX}
       end
 
       def generate_namespace_open
@@ -36,20 +38,77 @@ module IbRubyProxy
       end
 
       def generate_class_declaration
-        struct_init_properties = ruby_property_attributes.collect{|property| ":#{property}"}.join(', ')
+        struct_init_properties = ruby_properties.collect {|property| ":#{property}"}.join(', ')
         struct_init_properties << ", keyword_init: true"
-        "#{class_name} = Struct.new(#{struct_init_properties})"
+
+        <<-RUBY
+          #{class_name} = Struct.new(#{struct_init_properties}) do
+        RUBY
+      end
+
+      def generate_constructor
+        constructor_declarations = ruby_properties.zip(java_property_fields).collect do |ruby_property, java_field|
+          "#{ruby_property}: #{default_value_for_java_field(java_field)}"
+        end
+
+        assignment_statements = ruby_properties.collect do |ruby_property|
+          <<-RUBY
+            self.#{ruby_property} = #{ruby_property}
+          RUBY
+        end
+
+        <<-RUBY
+            def initialize(#{constructor_declarations.join(', ')})
+              #{assignment_statements.join('')}        
+            end
+        RUBY
+      end
+
+      def default_value_for_java_field(java_field)
+        case java_field.type
+        when Java::int.java_class, Java::float.java_class, Java::double.java_class
+          '0'
+        when Java::boolean.java_class
+          'false'
+        else
+          'nil'
+        end
       end
 
       def class_name
         ib_class.name.split('::').last
       end
 
-      def ruby_property_attributes
-        java_property_fields.collect do |field|
-          field_name = field.name.gsub(IB_FIELD_PREFIX, '')
+      def ruby_properties
+        @ruby_properties ||= java_property_fields.collect do |field|
+          field_name = field_name(field)
           to_underscore(field_name)
         end
+      end
+
+      # Sometimes the case of the private method and the accessor does not match
+      def field_name(field)
+        field_name_without_prefix = field.name.gsub(IB_FIELD_PREFIX, '')
+        method = ib_class.java_class.declared_instance_methods.find {|method| method.name.downcase == field_name_without_prefix.downcase}
+        raise "No method matching '#{field.name}'?" if !method
+        method.name
+      end
+
+      def generate_to_ib_method
+        property_copy_sentences = ruby_properties.collect do |ruby_property|
+          java_setter = to_camel_case(ruby_property)
+          <<-RUBY
+            ib_object.#{java_setter}(#{ruby_property})
+          RUBY
+        end
+
+        <<-RUBY
+          def to_ib
+            ib_object = #{ib_class.name}.new
+            #{property_copy_sentences.join('')}
+            ib_object             
+          end
+        RUBY
       end
     end
   end
