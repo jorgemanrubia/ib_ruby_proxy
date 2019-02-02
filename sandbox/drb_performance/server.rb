8c4091bdf58859148f781d368f65f3fb79b5cc57
Jorge Manrubia <jorge.manrubia@gmail.com>
2019-02-02 11:10:22 +0100

    Add dummy performance test for drb

---
+++ sandbox/drb_performance/server.rb
@@ -0,0 +1,26 @@
+require 'drb'
+require 'drb/observer'
+
+class Server
+  include DRb::DRbObservable
+  attr_accessor :object
+
+  def initialize()
+    @object = {:id => 'from server side'}
+  end
+
+  def add(numnber1, number2)
+    changed
+    notify_observers(numnber1, number2, numnber1 + number2)
+  end
+end
+
+server = Server.new
+
+DRb.start_service('druby://localhost:1992', server)
+
+begin
+  DRb.thread.join
+rescue Exception
+  Process.kill("TERM", Process.pid)
+end
