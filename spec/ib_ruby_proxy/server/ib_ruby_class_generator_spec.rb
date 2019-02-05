525d4a1df3f96ad00f227c8c549a4b0d02923090
Jorge Manrubia <jorge.manrubia@gmail.com>
2019-02-05 21:46:59 +0100

    Extend java classes with #to_ruby instead of adding class method

--- spec/ib_ruby_proxy/server/ib_ruby_class_generator_spec.rb
+++ spec/ib_ruby_proxy/server/ib_ruby_class_generator_spec.rb
@@ -12,15 +12,47 @@ describe IbRubyProxy::Server::IbRubyClassGenerator do
     end
   end
 
-  describe "Generated methods" do
+  describe "Extended ib class" do
     before(:context) do
       generator = IbRubyProxy::Server::IbRubyClassGenerator.new(Contract, namespace: 'IbRubyProxy::Client::Ib::Test2')
+      puts generator.ruby_class_source
+      eval generator.ruby_class_source
+    end
+
+
+    describe '#to_ruby' do
+      it 'should add a method to ruby that converts the ib object into a ruby object' do
+        puts Contract.name
+
+        ib_contract = Java::ComIbClient::Contract.new
+        ib_contract.symbol('ES')
+        ib_contract.secType('FUT')
+        ib_contract.currency('USD')
+        ib_contract.exchange('GLOBEX')
+        ib_contract.lastTradeDateOrContractMonth('201903')
+
+        ruby_contract = ib_contract.to_ruby
+
+        expect(ruby_contract).to be_instance_of(IbRubyProxy::Client::Ib::Test2::Contract)
+        expect(ruby_contract.symbol).to eq('ES')
+        expect(ruby_contract.sec_type).to eq(Types::SecType::FUT)
+        expect(ruby_contract.currency).to eq('USD')
+        expect(ruby_contract.exchange).to eq('GLOBEX')
+        expect(ruby_contract.last_trade_date_or_contract_month).to eq('201903')
+      end
+    end
+  end
+
+
+  describe "Generated ruby class" do
+    before(:context) do
+      generator = IbRubyProxy::Server::IbRubyClassGenerator.new(Contract, namespace: 'IbRubyProxy::Client::Ib::Test3')
       eval generator.ruby_class_source
     end
 
     describe "#to_ib" do
       it 'creates an ib object with simple attributes copied' do
-        contract = IbRubyProxy::Client::Ib::Test2::Contract.new symbol: 'ES',
+        contract = IbRubyProxy::Client::Ib::Test3::Contract.new symbol: 'ES',
                                                                 sec_type: 'FUT',
                                                                 currency: 'USD',
                                                                 exchange: 'GLOBEX',
@@ -32,25 +64,6 @@ describe IbRubyProxy::Server::IbRubyClassGenerator do
         expect(ib_contract.currency).to eq('USD')
         expect(ib_contract.exchange).to eq('GLOBEX')
         expect(ib_contract.lastTradeDateOrContractMonth).to eq('201903')
-      end
-    end
-
-    describe '.from_ib' do
-      it 'creates a ruby object from with the attributes copied from the provided IB object' do
-        ib_contract = Java::ComIbClient::Contract.new
-        ib_contract.symbol('ES')
-        ib_contract.secType('FUT')
-        ib_contract.currency('USD')
-        ib_contract.exchange('GLOBEX')
-        ib_contract.lastTradeDateOrContractMonth('201903')
-
-        ruby_contract = IbRubyProxy::Client::Ib::Test2::Contract.from_ib(ib_contract)
-        expect(ruby_contract).to be_instance_of(IbRubyProxy::Client::Ib::Test2::Contract)
-        expect(ruby_contract.symbol).to eq('ES')
-        expect(ruby_contract.sec_type).to eq(Types::SecType::FUT)
-        expect(ruby_contract.currency).to eq('USD')
-        expect(ruby_contract.exchange).to eq('GLOBEX')
-        expect(ruby_contract.last_trade_date_or_contract_month).to eq('201903')
       end
     end
   end
