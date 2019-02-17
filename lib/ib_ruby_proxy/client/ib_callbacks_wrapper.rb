0871bd3a55dcbf39c4f639cb230320f1fdcf58da
Jorge Manrubia <jorge.manrubia@gmail.com>
2019-02-17 21:45:23 +0100

    Add spec

--- lib/ib_ruby_proxy/client/ib_callbacks_wrapper.rb
+++ lib/ib_ruby_proxy/client/ib_callbacks_wrapper.rb
@@ -1,13 +1,16 @@
-
 module IbRubyProxy
   module Client
     class IbCallbacksWrapper
+      include IbRubyProxy::Util::HasLogger
+
+      # This is the ruby equivalent to Interactive Brokers EWrapper class. Extend it and add the methods
+      # you want to respond to.
       include DRb::DRbUndumped
 
       def update(*params)
-        puts "UPDATE RECEIVED IN OBSERVER"
         method_name, *arguments = params
-        send method_name, arguments
+        logger.debug "Method received in wrapper: #{method_name}"
+        send method_name, *arguments
       end
     end
   end
