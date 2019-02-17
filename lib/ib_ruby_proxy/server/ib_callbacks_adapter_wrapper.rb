2351542f73365f447cf4c513dfb7a58a695d008f
Jorge Manrubia <jorge.manrubia@gmail.com>
2019-02-17 22:15:40 +0100

    Add spec and tidy up a bit

--- lib/ib_ruby_proxy/server/ib_callbacks_adapter_wrapper.rb
+++ lib/ib_ruby_proxy/server/ib_callbacks_adapter_wrapper.rb
@@ -3,11 +3,15 @@ require 'drb/observer'
 
 module IbRubyProxy
   module Server
+    # EWrapper implementation that delegates responding to each method by triggering an observer notification. It will
+    # translate the name of the java method to Ruby (camelcase), as well as translate the parameters from ib to the
+    # ruby world.
     class IbCallbacksAdapterWrapper
       # include DRb::DRbObservable
       include EWrapper
       include DRb::DRbObservable
       extend IbRubyProxy::Util::StringUtils
+      extend IbRubyProxy::Util::HasLogger
 
       attr_reader :signal, :client
 
@@ -16,39 +20,24 @@ module IbRubyProxy
         @client = EClientSocket.new(self, @signal)
       end
 
-      EWrapper.java_class.declared_instance_methods.each do |java_method|
+      def self.define_ruby_method_for(java_method)
         ruby_method_name = to_underscore(java_method.name)
-
         class_eval <<-RUBY
           def #{java_method.name}(*arguments)
             ruby_arguments = arguments.collect(&:to_ruby)
             if count_observers > 0
-              puts "****"
-              puts "RECEIVED WITH WRAPPED"
-              puts "#{ruby_method_name}"
-              ap ruby_arguments
-              puts "****"
-              
               changed
               notify_observers *(["#{ruby_method_name}"] + ruby_arguments)
             else
-              handle_callback_when_no_wrapper("#{ruby_method_name}", *ruby_arguments)
+              logger.debug "Received #{ruby_method_name}"
+              logger.debug ruby_arguments.inspect
             end
           end
-
-          def handle_callback_when_no_wrapper(method_name, *arguments)
-puts "*" 
-puts "RECEIVED GENERIC"
-puts method_name
-puts arguments
-raise arguments.last if arguments.last.respond_to?(:backtrace)
-
-puts "*" 
-            # name, rest_of_arguments = arguments
-            # puts name
-            # puts rest_of_arguments
-          end
         RUBY
+      end
+
+      EWrapper.java_class.declared_instance_methods.each do |java_method|
+        define_ruby_method_for(java_method)
       end
     end
 
