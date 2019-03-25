5f4ef13d136576052b69f10afd5be0107a34d870
Jorge Manrubia <jorge.manrubia@gmail.com>
2019-03-25 23:14:39 +0100

    Clarify example

--- examples/client.rb
+++ examples/client.rb
@@ -12,8 +12,7 @@ def emini
                                         last_trade_date_or_contract_month: '201906'
 end
 
-promise = client.req_historical_ticks(18004, emini, "20190320 21:39:33", nil, 100, "TRADES", 1, false, nil)
-promise.then do |id, ticks, done|
+client.req_historical_ticks(18004, emini, "20190320 21:39:33", nil, 100, "TRADES", 1, false, nil).then do |id, ticks, done|
   ap ticks
 end
 
