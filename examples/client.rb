490eb291727b228af664f91cf4722c2b0a53801d
Jorge Manrubia <jorge.manrubia@gmail.com>
2019-03-25 23:14:21 +0100

    Tweak example

--- examples/client.rb
+++ examples/client.rb
@@ -4,33 +4,18 @@ require 'concurrent-ruby'
 
 client = IbRubyProxy::Client::Client.new
 
-class MyWrapper < IbRubyProxy::Client::IbCallbacksWrapper
-  def error(*arguments)
-    puts "ERROR RECEIVED"
-    ap arguments
-  end
-
-  def historical_ticks_last(*arguments)
-    puts "HISTORICAL!!!"
-    puts arguments.inspect
-  end
-end
-
-
 def emini
-  contract = IbRubyProxy::Client::Ib::Contract.new symbol: 'ES',
-                                                   sec_type: 'FUT',
-                                                   currency: 'USD',
-                                                   exchange: 'GLOBEX',
-                                                   last_trade_date_or_contract_month: '201906'
-  contract
+  IbRubyProxy::Client::Ib::Contract.new symbol: 'ES',
+                                        sec_type: 'FUT',
+                                        currency: 'USD',
+                                        exchange: 'GLOBEX',
+                                        last_trade_date_or_contract_month: '201906'
 end
 
-# client.add_ib_callbacks_wrapper MyWrapper.new
 promise = client.req_historical_ticks(18004, emini, "20190320 21:39:33", nil, 100, "TRADES", 1, false, nil)
 promise.then do |id, ticks, done|
-  puts "PROMISE RESOLVED"
   ap ticks
 end
 
-sleep
+promise.value # block until promise resolved
+
