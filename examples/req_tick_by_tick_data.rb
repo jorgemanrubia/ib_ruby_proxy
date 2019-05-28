0c4a32fcf76666229d0970a5d8713293041596b3
Jorge Manrubia <jorge.manrubia@gmail.com>
2019-05-28 21:50:43 +0200

    Remove blank line

--- examples/req_tick_by_tick_data.rb
+++ examples/req_tick_by_tick_data.rb
@@ -2,7 +2,6 @@ require_relative './common'
 
 client = IbRubyProxy::Client::Client.from_drb
 
-
 client.req_tick_by_tick_data(18002, Securities.emini, 'Last', 0, false) do |*arguments|
   ap arguments
 end
