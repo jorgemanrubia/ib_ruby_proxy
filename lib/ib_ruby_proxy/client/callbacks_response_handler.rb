a2c79d79ef16f4fea70c01f5cebee7b05cd8952b
Jorge Manrubia <jorge.manrubia@gmail.com>
2019-06-12 00:18:22 +0200

    Vastly simplify response handlers approach
    - We are only going to use regular callbacks.
    - It will always pass the method name
    
    That's it

--- lib/ib_ruby_proxy/client/callbacks_response_handler.rb
+++ lib/ib_ruby_proxy/client/callbacks_response_handler.rb
@@ -13,24 +13,23 @@ module IbRubyProxy
 
       def callback_received(callback_name, *arguments)
         callback_name = callback_name.to_sym
-        callback_handlers[callback_name]&.callback_received(*arguments, callback_name: callback_name)
+        callback_handlers[callback_name]&.callback_received(callback_name, *arguments)
       end
 
       # @todo: Move outside of this class when we add more options
       def self.for_ib
         self.new.tap do |handler|
-          handler.configure_single_response_promise_callback method: :req_historical_ticks,
-                                                             callback: %i(historical_ticks historical_ticks_bid_ask historical_ticks_last error),
-                                                             discriminate_by_argument_nth: 0
+          handler.configure_block_callback method: :req_historical_ticks,
+                                           callback: %i(historical_ticks historical_ticks_bid_ask historical_ticks_last),
+                                           discriminate_by_argument_nth: 0
 
-          handler.configure_multi_response_promise_callback method: :req_contract_details,
-                                                            callback: %i(contract_details error),
-                                                            done_callback: :contract_details_end,
-                                                            discriminate_by_argument_nth: 0
+          handler.configure_block_callback method: :req_contract_details,
+                                           callback: %i(contract_details contract_details_end),
+                                           discriminate_by_argument_nth: 0
 
           handler.configure_block_callback method: :req_tick_by_tick_data,
-                                                            callback: %i(tick_by_tick_bid_ask tick_by_tick_all_last tick_by_tick_mid_point error),
-                                                            discriminate_by_argument_nth: 0
+                                           callback: %i(tick_by_tick_bid_ask tick_by_tick_all_last tick_by_tick_mid_point),
+                                           discriminate_by_argument_nth: 0
 
           handler.configure_block_callback method: :req_positions,
                                            callback: %i(position error),
@@ -38,28 +37,9 @@ module IbRubyProxy
 
           # @todo this is wrong, just for debugging...
           handler.configure_block_callback method: :req_account_updates,
-                                                            callback: %i(update_account_value update_portfolio update_account_time error),
-                                                            discriminate_by_argument_nth: 0
+                                           callback: %i(update_account_value update_portfolio update_account_time),
+                                           discriminate_by_argument_nth: 0
         end
-      end
-
-      def configure_single_response_promise_callback(method:, callback:, discriminate_by_argument_nth: 0)
-        validate_can_add_callback_on_method!(method)
-
-        handler = PromiseSingleResponseHandler.new(discriminate_by_argument_nth)
-
-        configure_callback_handler(callback, handler)
-        configure_method_handler(method, handler)
-      end
-
-      def configure_multi_response_promise_callback(method:, callback:, done_callback:, discriminate_by_argument_nth: 0)
-        validate_can_add_callback_on_method!(method)
-
-        handler = PromiseMultipleResponseHandler.new(discriminate_by_argument_nth, done_callback)
-
-        configure_callback_handler(callback, handler)
-        configure_callback_handler(done_callback, handler)
-        configure_method_handler(method, handler)
       end
 
       def configure_block_callback(method:, callback:, discriminate_by_argument_nth:)
@@ -81,6 +61,7 @@ module IbRubyProxy
 
       def configure_callback_handler(callback, handler)
         callback = [callback] unless callback.respond_to?(:each)
+        callback << :error
         callback.each do |callback_name|
           callback_handlers[callback_name.to_sym] = handler
         end
@@ -88,72 +69,6 @@ module IbRubyProxy
 
       def validate_can_add_callback_on_method!(method)
         raise "Already configured handler for #{method}" if method_handlers[method]
-      end
-
-      class PromiseSingleResponseHandler
-        include IbRubyProxy::Util::HasLogger
-
-        attr_reader :discriminate_by_argument_nth, :promise
-
-        def initialize(discriminate_by_argument_nth)
-          @discriminate_by_argument_nth = discriminate_by_argument_nth
-          @promises_by_key = {}
-        end
-
-        def method_invoked(*arguments)
-          key = arguments[discriminate_by_argument_nth]
-          raise "Configured with a promise and invoked more than once?" if @promises_by_key[key]
-          @promises_by_key[key] = Concurrent::Promises.resolvable_future
-        end
-
-        def callback_received(*arguments, callback_name: nil)
-          promise = promise_for_arguments(arguments)
-          if callback_name.to_s == 'error'
-            reject_promise_on_error(promise, arguments)
-          else
-            promise&.fulfill arguments
-          end
-        end
-
-        protected
-
-        def promise_for_arguments(arguments)
-          key = arguments[discriminate_by_argument_nth]
-          @promises_by_key[key]
-        end
-
-        private
-
-        def reject_promise_on_error(promise, arguments)
-          logger.error "Error received when handling response: #{arguments.inspect}"
-          promise&.reject arguments
-        end
-      end
-
-      class PromiseMultipleResponseHandler < PromiseSingleResponseHandler
-        attr_reader :discriminate_by_argument_nth, :promise, :done_callback
-
-        def initialize(discriminate_by_argument_nth, done_callback)
-          super(discriminate_by_argument_nth)
-          @done_callback = done_callback
-          @promises_by_key = {}
-          @results_by_key = {}
-        end
-
-        def callback_received(*arguments, callback_name: nil)
-          promise = promise_for_arguments(arguments)
-          key = arguments[discriminate_by_argument_nth]
-
-          case callback_name.to_s
-          when done_callback.to_s
-            promise&.fulfill @results_by_key[key]
-          when 'error'
-            reject_promise_on_error(promise, arguments)
-          else
-            @results_by_key[key] ||= []
-            @results_by_key[key] << arguments
-          end
-        end
       end
 
       class CallbackResponseHandler
@@ -169,11 +84,11 @@ module IbRubyProxy
           @block = block
         end
 
-        def callback_received(*arguments, callback_name: nil)
+        def callback_received(callback_name, *arguments)
           if callback_name.to_s == 'error'
             raise StandardError, arguments.join('. ')
           else
-            @block&.call(*arguments)
+            @block&.call(callback_name, *arguments)
           end
         end
       end
--- spec/ib_ruby_proxy/client/callbacks_response_handler_spec.rb
+++ spec/ib_ruby_proxy/client/callbacks_response_handler_spec.rb
@@ -3,49 +3,6 @@ describe IbRubyProxy::Client::CallbacksResponseHandler do
     subject(:callbacks_response_handler) { IbRubyProxy::Client::CallbacksResponseHandler.new }
     let(:client) { double 'dummy client', callbacks_response_handler: callbacks_response_handler, error: nil }
 
-    describe '#configure_single_response_promise_callback' do
-      it 'allows handling callbacks as promises by matching them via a param' do
-        callbacks_response_handler.configure_single_response_promise_callback method: :some_method,
-                                                                              callback: :response_callback,
-                                                                              discriminate_by_argument_nth: 0
-
-        def client.some_method(id, name)
-          callbacks_response_handler.method_invoked(:some_method, id, name)
-        end
-
-        promise = client.some_method(1, 'some param')
-        expect(promise).not_to be_resolved
-
-        callbacks_response_handler.callback_received(:response_callback, 1, 'value 1', 'value 2')
-
-        expect(promise).to be_resolved
-        expect(promise.value).to eq([1, 'value 1', 'value 2'])
-      end
-    end
-
-    describe '#configure_multi_response_promise_callback' do
-      it 'allows handling multiple callbacks and collecting the results until a finish callback is invoked' do
-        callbacks_response_handler.configure_multi_response_promise_callback method: :some_method,
-                                                                             done_callback: :done,
-                                                                             callback: :response_callback,
-                                                                             discriminate_by_argument_nth: 0
-
-        def client.some_method(id, name)
-          callbacks_response_handler.method_invoked(:some_method, id, name)
-        end
-
-        promise = client.some_method(1, 'some param')
-        expect(promise).not_to be_resolved
-
-        callbacks_response_handler.callback_received(:response_callback, 1, 'value 1-1', 'value 1-2')
-        callbacks_response_handler.callback_received(:response_callback, 1, 'value 2-1', 'value 2-2')
-        callbacks_response_handler.callback_received(:done, 1)
-
-        expect(promise).to be_resolved
-        expect(promise.value).to eq([[1, 'value 1-1', 'value 1-2'], [1, 'value 2-1', 'value 2-2']])
-      end
-    end
-
     describe '#configure_block_callback' do
       let(:proc) { ->{} } # getting stackoverflow error with proc{} so using a lambda instead
 
@@ -60,9 +17,9 @@ describe IbRubyProxy::Client::CallbacksResponseHandler do
 
         client.some_method(1, 'some param', &proc)
 
-        expect(proc).to receive(:call).with(1, 'value 1-1', 'value 1-2').ordered
-        expect(proc).to receive(:call).with(1, 'value 1-3', 'value 1-4').ordered
-        expect(proc).to receive(:call).with(2, 'value 2-1', 'value 2-2').ordered
+        expect(proc).to receive(:call).with(:response_callback, 1, 'value 1-1', 'value 1-2').ordered
+        expect(proc).to receive(:call).with(:response_callback, 1, 'value 1-3', 'value 1-4').ordered
+        expect(proc).to receive(:call).with(:response_callback, 2, 'value 2-1', 'value 2-2').ordered
 
         callbacks_response_handler.callback_received(:response_callback, 1, 'value 1-1', 'value 1-2')
         callbacks_response_handler.callback_received(:response_callback, 1, 'value 1-3', 'value 1-4')
