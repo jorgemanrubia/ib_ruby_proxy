f91d6375deb9ca9850ceb16973b82abbac52e77d
Jorge Manrubia <jorge.manrubia@gmail.com>
2019-06-11 22:14:40 +0200

    Adapt examples to the new simplified approach

--- examples/req_historical_ticks.rb
+++ examples/req_historical_ticks.rb
@@ -2,11 +2,11 @@ require_relative './common'
 
 client = IbRubyProxy::Client::Client.from_drb
 
-promise = client.req_historical_ticks(18009, Securities.emini, nil, "20190304 17:00:01", 100, "MIDPOINT", 1, false, nil).then do |id, ticks, done|
+client.req_historical_ticks(18009, Securities.emini, nil, "20190304 17:00:01", 100, "MIDPOINT", 1, false, nil) do |_callback, _request_id, ticks, done|
   ap ticks
 end
 
-promise.value # block until promise resolved
+sleep
 
 
 
