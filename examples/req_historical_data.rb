287765d6337fb4a0789f7dbf24d9f3fe98fe6c77
Jorge Manrubia <jorge.manrubia@gmail.com>
2019-06-19 10:35:30 +0200

    Add support for requesting historical bars

---
+++ examples/req_historical_data.rb
@@ -0,0 +1,14 @@
+require_relative './common'
+
+client = IbRubyProxy::Client::Client.from_drb
+
+client.req_historical_data(18009, Securities.emini, "20190304 17:00:01", "1 M", "1 day", "TRADES", 1, 1, false, nil) do |_callback, _request_id, bar|
+  ap bar
+end
+
+sleep
+
+
+
+
+
