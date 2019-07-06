d10938221d0a6ecef80f8976dffb77f4b9f533bf
Jorge Manrubia <jorge.manrubia@gmail.com>
2019-07-06 07:56:52 +0200

    Add documentation

--- lib/ib_ruby_proxy/server/reflection/ib_class.rb
+++ lib/ib_ruby_proxy/server/reflection/ib_class.rb
@@ -1,6 +1,7 @@
 module IbRubyProxy
   module Server
     module Reflection
+      # An ib class
       class IbClass
         include IbRubyProxy::Util::StringUtils
 
@@ -10,26 +11,41 @@ module IbRubyProxy
           @klass = klass
         end
 
+        # The class name without including namespaces
+        #
+        # @return [String]
         def name
           klass.name.split('::').last
         end
 
+        # The full qualified class name including namespace
+        #
+        # @return [Object]
         def full_name
           klass.name
         end
 
+        # List of ib fields that represent properties to interchange
+        #
+        # @return [Array<IbField>]
         def java_property_fields
           @java_property_fields ||= java_fields.collect { |field| IbField.new(field, self) }
         end
 
-        def ruby_properties
+        # List of ruby properties names that correspond to {#java_property_fields}
+        #
+        # @return [Array<String>]
+        def ruby_property_names
           @ruby_properties ||= java_property_fields.collect do |field|
             to_underscore(field.name)
           end
         end
 
+        # Return an array of combinations of {#ruby_property_names} and #{java_property_fields}
+        #
+        # @return [Array<Array<String, IbField>>]
         def zipped_ruby_and_java_properties
-          ruby_properties.zip(java_property_fields)
+          ruby_property_names.zip(java_property_fields)
         end
 
         private
