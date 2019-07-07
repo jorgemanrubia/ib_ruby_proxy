c6fd090fd9a032fc61470145eb7e03fe4482e7f0
Jorge Manrubia <jorge.manrubia@gmail.com>
2019-07-07 07:48:53 +0200

    Add spec for requesting historical ticks

---
+++ spec/ib_ruby_proxy/system/req_historical_ticks_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe '#req_historical_ticks', :impersonator do
+  let(:client) { build_impersonated_client }
+  let(:contract) { Test::Securities.emini }
+
+  it 'returns the list of ticks' do
+    promise = Concurrent::Promises.resolvable_future.tap do |future|
+      client.req_historical_ticks(18001, contract, nil, '20190304 17:00:01', 10,
+          'MIDPOINT', 1, false, nil) do |_callback, _request_id, ticks, _done|
+        future.fulfill(ticks)
+      end
+    end
+    ticks = promise.value
+    expect(ticks.length).to be >= 10
+    expect(ticks.last).to be_a(IbRubyProxy::Client::Ib::HistoricalTick)
+    expect(ticks.last.price).to be >= 0
+  end
+end
